358461a9638f7d760912070b715316a641df9484
Nicolas <n.kaletsch@gmail.com>
2025-03-17 14:43:00 +0100

    add test for SIF examples

---
+++ test/main/vct/test/integration/examples/SIFExamplesSpec.scala
@@ -0,0 +1,10 @@
+package vct.test.integration.examples
+
+import vct.test.integration.helper.VercorsSpec
+
+class SIFExamplesSpec extends VercorsSpec {
+  vercors should verify using anyBackend example "concepts/sif/LowLoopExample.java"
+  vercors should fail withCode "postFailed:false" using anyBackend  example "concepts/sif/SimpleLowExample.java"
+  vercors should verify using anyBackend example "concepts/sif/nagini-examples/TestTryCatch.java"
+  vercors should fail withCode "postFailed:false" using anyBackend example "concepts/sif/nagini-examples/TestTryCatchFailing.java"
+}
